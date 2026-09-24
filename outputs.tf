2d438d1bb33fee83dc853be093aac35cfa87f5a3
pawel150199 <pawel.polski@gmail.com>
2026-09-24 16:35:57 +0200

    chore: release version 0.1.8

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,9 @@
-output "kubernetes_network_ip_range" {
-  description = "Kubernetes network IP range"
-  value       = hcloud_network.private_network.ip_range
+output "master_nodes_ips" {
+  description = "Master Nodes IP Addresses"
+  value       = hcloud_server.master_nodes[*].ipv4_address
 }
 
-output "master_node_ip" {
-  description = "Master Node IP Address"
-  value       = hcloud_server.master_nodes[0].ipv4_address
-}
-
-output "master_node_ssh_public_key" {
-  description = "Public key of the SSH key pair generated for the master node's cluster user"
-  value       = tls_private_key.master_node.public_key_openssh
-}
-
-output "master_node_ssh_private_key" {
-  description = "Private key matching master_node_ssh_public_key. Use it to SSH into the master as the cluster user."
-  value       = tls_private_key.master_node.private_key_openssh
-  sensitive   = true
-}
-
-output "worker_node_ssh_public_key" {
-  description = "Public key of the SSH key pair generated for the worker nodes' cluster user"
-  value       = tls_private_key.worker_node.public_key_openssh
-}
-
-output "worker_node_ssh_private_key" {
-  description = "Private key matching worker_node_ssh_public_key. Use it to SSH into the workers as the cluster user."
-  value       = tls_private_key.worker_node.private_key_openssh
-  sensitive   = true
+output "worker_nodes_ips" {
+  description = "Worker Nodes IP Addresses"
+  value       = hcloud_server.worker_nodes[*].ipv4_address
 }
